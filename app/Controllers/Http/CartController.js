f080088dd5a622d5d06e0a5c0c528478bb03b5a4
Felipe <felipeperozo@hotmail.com>
2019-07-25 18:32:09 -0300

    commit screens fix initial cart arquiteture]

--- app/Controllers/Http/CartController.js
+++ app/Controllers/Http/CartController.js
@@ -3,19 +3,20 @@
 const Product = use("App/Models/Product");
 
 class CartController {
-  async addOn({ request, response, auth, session, params }) {
-    if (auth.user && auth.user.type == "client") {
-      const itensCart = session.get("itensCart") || [];
-      const product = await Product.findBy("id", params.id);
-      itensCart.push(product);
-      session.put("itensCart", itensCart);
-      response.send(itensCart);
+    async addOn({ request, response, auth, session, params }) {
+        const quantidade = 1;
+        if (auth.user && auth.user.type == "client") {
+            const itensCart = session.get("itensCart") || [];
+            const product = await Product.findBy("id", params.id);
+            itensCart.push({ product, quantidade });
+            session.put("itensCart", itensCart);
+            response.send(itensCart);
+        }
     }
-  }
 
-  async list({ request, response, view, data }) {
-    return view.render("cart", { data });
-  }
+    async list({ request, response, view, data }) {
+        return view.render("cart", { data });
+    }
 }
 
 module.exports = CartController;
