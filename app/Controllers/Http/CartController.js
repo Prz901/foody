98462c1fd6bedba0796cfb64e3eca9df4fb85f59
Felipe <felipeperozo@hotmail.com>
2019-07-30 18:05:41 -0300

    cart done

--- app/Controllers/Http/CartController.js
+++ app/Controllers/Http/CartController.js
@@ -5,7 +5,7 @@ const Order = use("App/Models/Order");
 
 class CartController {
     async create({ request, response, auth, session, params }) {
-        const quantity = 1;
+        const quantidade = 1;
         if (auth.user && auth.user.type == "client") {
             const itensCart = session.get("itensCart") || [];
             const product = await Product.findBy("id", params.id);
@@ -27,27 +27,16 @@ class CartController {
         return view.render("cart", { datas });
     }
 
-    async store({request, response, auth, view, session}) {
+    async store({ request, response, auth, view, session }) {
         if (auth.user && auth.user.type == "client") {
             const data = await session.get("itensCart");
             //console.log(data);
             data.id_users = auth.user.id;
             data.quantity = await request.only(["quantity"]);
             //console.log(data);
-            for(var i = 0; i < data.length; i++){
-                console.log(data.product);
-                /*const order = [];
-                order['id_products'] = data.id_products;
-                order['price'] = data.price;
-                order['quantity'] = data.quantity;
-                console.log(order);
-//                await Order.create(order);*/
-              
-                console.log("Armazenado");
-            }
-            
-            //console.log(order);
-          }  
+
+            console.log("Armazenado");
+        }
     }
 
     // remover um produto do carrinho
