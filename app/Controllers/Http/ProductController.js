8aaef65768759ee5a06b63481548c67300287f7c
AnnaFragoso <anna.barbara.fragoso@hotmail.com>
2019-08-07 16:35:19 -0300

    Upload da foto de produtos no create

--- app/Controllers/Http/ProductController.js
+++ app/Controllers/Http/ProductController.js
@@ -6,6 +6,7 @@
 
 const Product = use("App/Models/Product");
 const Category = use("App/Models/Category");
+const Helpers = use('Helpers')
 
 /**
  * Resourceful controller for interacting with products
@@ -51,7 +52,8 @@ class ProductController {
 
 
         return view.render("editproduct", { product, categories });
-      
+    }
+
       async store({ request, response, auth }) {
             if (auth.user && auth.user.type == "admin") {
                 const data = await request.only([
@@ -59,9 +61,18 @@ class ProductController {
                     "price",
                     "id_categories"
                 ]);
-                const image = request.file("image");
+                const images = await request.file('image', {
+                                            types: ['image'],
+                                            size: '2mb'
+                });
+               
+                await images.move(Helpers.tmpPath('../App/uploads'))
+
+                
                 data.id_users = auth.user.id;
+                data.image = '../App/uploads/' + images.fileName;
                 const product = await Product.create(data);
+                
                 return response.redirect("/product");
             }
         }
